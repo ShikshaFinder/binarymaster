39f900c1c99d97e17fe98bb5aa138032b74a9099
ShikshaFinder <janiharsh794@gmail.com>
2024-10-29 16:18:52 +0530

    link added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,7 +24,9 @@ export default function ImagesSliderDemo() {
           Create Stunning Digital Products <br /> Which your Users Love
         </motion.p>
         <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-          <a href="">
+          <a
+            href="https://wa.me/917984140706"
+          >
             <span>Connect Now →</span>
           </a>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
